6a2679a2dcb98dc4447af8eb453e297cd7585c79
Win Thurein Myint <harrychan2139@gmail.com>
2020-06-11 20:11:01 -0400

    declare global var

--- API/search_api.py
+++ API/search_api.py
@@ -1,40 +1,43 @@
+from src.network import NetworkSearcher
+from src.network import NetworkInterface
+from src.siamese_model import SiameseModel
+from flask import request, jsonify
 import json
 import pprint
 import datetime
 import flask
-from src.network import NetworkSearcher, NetworkInterface
-from src.siamese_model import SiameseModel
-from flask import request
 from flask_cors import CORS
+
+global model, scan, searcher
+model = SiameseModel('cpu')
+scan = NetworkInterface.create_connection()
+searcher = NetworkSearcher(model=model, network=scan)
+print("***********SET global DONE*************")
 
 app = flask.Flask(__name__)
 cors = CORS(app)
 
-
-# csn_search with input txt
-# search?inputText=<value>
 @app.route('/search', methods=['GET'])
 def search():
-    input_text = request.args.get('inputText')
-    model = SiameseModel('cpu')
-    scan = NetworkInterface.create_connection()
-    searcher = NetworkSearcher(model=model, network=scan)
-    start_time = datetime.datetime.now()
-    results = searcher(input_text, 2)
-    search_time = _search_time(start_time)
-    print(f"Search took {search_time[0]}.{search_time[1]} secs.\n")
-    print(results)
-    lists = []
-    for result in results:
-        lists.append(result)
-    results = json.dumps(results)
-    return results
-
+	print("** do search **")
+	input = request.args.get('inputText')
+	start_time = datetime.datetime.now()
+	results = searcher(input, 10)
+	search_time = _search_time(start_time)
+	print(f"Search took {search_time[0]}.{search_time[1]} secs.\n")
+	print(results)
+	lists=[]
+	for result in results:
+		pprint.pprint(result)
+		print()
+		lists.append(result)
+	results = json.dumps(results)
+	print(type(results))
+	return results;
 
 def _search_time(start_time):
-    delta = datetime.datetime.now() - start_time
-    return delta.seconds, delta.microseconds
-
+	delta = datetime.datetime.now() - start_time
+	return (delta.seconds, delta.microseconds)
 
 if __name__ == "__main__":
-    app.run()
+	app.run(host='0.0.0.0')
